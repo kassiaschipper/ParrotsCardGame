62e222e0ce813eeb0daaef6fec3627112af5ef92
Kássia <kassia016@gmail.com>
2022-06-19 00:35:07 -0300

    Function that turn up the cards and compare them

--- script.js
+++ script.js
@@ -1,17 +1,25 @@
 
 const faceCards = [
-    '<img src="imagens\\bobrossparrot.gif"></img>',
-    '<img src="imagens\\explodyparrot.gif"></img>',
-    '<img src="imagens\\fiestaparrot.gif"></img>',
-    '<img src="imagens\\metalparrot.gif"></img>',
-    '<img src="imagens\\revertitparrot.gif"></img>',
-    '<img src="imagens\\tripletsparrot.gif"></img>',
-    '<img src="imagens\\unicornparrot.gif"></img>'
+    '<img class="images" src="imagens\\bobrossparrot.gif"></img>',
+    '<img class="images" src="imagens\\explodyparrot.gif"></img>',
+    '<img class="images" src="imagens\\fiestaparrot.gif"></img>',
+    '<img class="images" src="imagens\\metalparrot.gif"></img>',
+    '<img class="images" src="imagens\\revertitparrot.gif"></img>',
+    '<img class="images" src="imagens\\tripletsparrot.gif"></img>',
+    '<img class="images" src="imagens\\unicornparrot.gif"></img>'
 ]
 
 let cardsOnTheBoard = 0;
 let deckOfCards = [];
 let numberOfCards = 0;
+// let numberOfMoves = 0;
+// let moves = 0;
+let faceUpCards = [];
+let firstCardUp = false;
+let secondCardUp = false;
+let matchingPair = 0
+let numberOfMatchings = 0;
+let click = 0;
 
 function verifyNumberOfCards() {
     numberOfCards = prompt(" De 4 a 14, com quntas cartas você quer jogar? Lembre-se de escolher um número par!");
@@ -20,23 +28,33 @@ function verifyNumberOfCards() {
     while (numberOfCards < 4 || numberOfCards > 14 || numberOfCards % 2 !== 0 || numberOfCards === isNaN) {
         numberOfCards = prompt("Você precisa escolher um número válido entre 4 e 14!")
     }
-
+    let teste = ["testwe", "testi", "tests", "testa", "trea"];
+    teste.sort(comparador);
+    console.log(teste);
     startingTheGame();
 }
 
 
 function startingTheGame() {
 
-    faceCards.sort(comparador);
+    // faceCards.sort(comparador)
+
     cardsOnTheBoard = numberOfCards / 2; //Pega o número de cartas escolhido pelo usuário e divide por dois para poder formar o par.
 
     for (let index = 0; index < cardsOnTheBoard; index++) { //percorre a lista embaralhada de gifs e adicionas no deck (pelo index) a quantidade referente a metade do numero digitado pelo usuario
         deckOfCards.push(faceCards[index]);
         deckOfCards.push(faceCards[index]);
+
+    }
+    deckOfCards.sort(comparador);//embaralha o deck com todos os pares
+    for (let index = 0; index < numberOfCards; index++) { //percorre a lista embaralhada de gifs e adicionas no deck (pelo index) a quantidade referente a metade do numero digitado pelo usuario
+
         toPutTheCards(index);
     }
     // alert (deckOfCards);
-    deckOfCards.sort(comparador);//embaralha o deck com todos os pares
+
+
+    console.log(deckOfCards);
 
 }
 
@@ -49,8 +67,41 @@ function toPutTheCards(index) {// aula de quinta
     let container = document.querySelector(".container");
     container.innerHTML += ` 
         <div class="card" onclick="turnTheCard(this);">
-        <div class="face-back-card"><img src="imagens\\front.png"></div>
-        <div class="face-card">${deckOfCards[index]}</div>
+        <div class="face-back-card"><img  src="imagens\\front.png"></div>
+        <div class="face-card hidden">${deckOfCards[index]}</div>
     </div>`;
 }
 
+function turnTheCard(card) {
+    numberOfMatchings = numberOfCards / 2;
+
+    click++;
+    
+    if(click < 3){
+        
+        let backCard = card.querySelector(".face-back-card ");
+        let faceCard = card.querySelector(".face-card ");
+
+        backCard.classList.add("hidden");
+        faceCard.classList.remove("hidden");
+
+        faceUpCards.push(faceCard);
+        
+        alert("teste");
+            
+            if(click === 2){
+                comparePair();
+            }    
+    }
+
+}
+
+function comparePair () {
+   
+    let firstImage = faceUpCards[0].querySelector(".images");
+    let secondImage = faceUpCards[1].querySelector(".images");
+
+    if(firstImage.src === secondImage.src){
+        alert("wooee");
+    }
+}
